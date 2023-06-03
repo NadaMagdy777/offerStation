ddf2124aab22560bdbf3d7161ff06f9ed5975099
Elshaymaa Ahmed <elshaymaaahmed@gmail.com>
2023-06-03 08:16:31 +0300

    BackEnd Setup

---
+++ offerStation_BackEnd/offerStation.Core/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using offerStation.Core.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace offerStation.Core.Interfaces.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        T GetById(int id);
+        Task<T> GetByIdAsync(int id);
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        T Find(Expression<Func<T, bool>> criteria, List<Expression<Func<T, object>>> includes = null);
+        Task<T> FindAsync(Expression<Func<T, bool>> criteria, List<Expression<Func<T, object>>> includes = null);
+        IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, List<Expression<Func<T, object>>> includes = null);
+        IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, int? take, int? skip,
+            Expression<Func<T, object>> orderBy = null, string orderByDirection = OrderBy.Ascending);
+        Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, List<Expression<Func<T, object>>> includes = null);
+        Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, int? skip, int? take,
+            Expression<Func<T, object>> orderBy = null, string orderByDirection = OrderBy.Ascending);
+        Task<T> Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ offerStation_BackEnd/offerStation.EF/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using offerStation.Core.Interfaces;
+using offerStation.EF.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace offerStation.EF
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
