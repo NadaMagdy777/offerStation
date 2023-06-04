fd5b56fc75c36ea0c3393b950a391431e55874a1
Youstina <tenarafaat1234@gmail.com>
2023-06-04 10:01:23 +0200

    update

--- offerStation_BackEnd/offerStation.Core/Models/CustomerCart.cs
+++ offerStation_BackEnd/offerStation.Core/Models/CustomerCart.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace offerStation.Core.Models
 {
-    internal class CustomerCart
+    public class CustomerCart
     {
+        [Key]
+        public int Id { get; set; }
+        
     }
 }
--- offerStation_BackEnd/offerStation.Core/Models/SupplierMenu.cs
+++ offerStation_BackEnd/offerStation.Core/Models/SupplierMenu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,8 @@ namespace offerStation.Core.Models
 {
     internal class SupplierMenu
     {
+        [Key]
+        public int Id { get; set; }
+
     }
 }
