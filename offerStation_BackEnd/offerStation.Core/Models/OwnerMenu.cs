cdf5734873f2c43af2a2b09c7511386404fd9c39
MayarANageeb <mayar.18332504@compit.aun.edu.eg>
2023-06-04 10:47:11 +0300

    create OwnerMenu,SupplierOffer,SupplierOfferProduct

---
+++ offerStation_BackEnd/offerStation.Core/Models/OwnerMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace offerStation.Core.Models
+{
+    internal class OwnerMenu
+    {
+        public int ID { get; set; }
+        public int OwnerID { get; set; }
+        public string MenuName { get; set; }
+        public string Description { get; set; }
+
+
+
+    }
+}
---
+++ offerStation_BackEnd/offerStation.Core/Models/SupplierOfferProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace offerStation.Core.Models
+{
+    internal class SupplierOfferProduct
+    {
+        public  int ID { get; set; }
+        public int OfferID { get; set; }
+        public int SupplierMenuID { get; set; }
+
+
+    }
+}
