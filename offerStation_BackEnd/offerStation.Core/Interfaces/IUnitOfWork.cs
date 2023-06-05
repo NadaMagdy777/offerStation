e1dd97a4c66468143643bdb958723e108de03f10
Elshaymaa Ahmed <elshaymaaahmed@gmail.com>
2023-06-05 11:53:42 +0300

    Edit UnitOfWork

--- offerStation_BackEnd/offerStation.Core/Interfaces/IUnitOfWork.cs
+++ offerStation_BackEnd/offerStation.Core/Interfaces/IUnitOfWork.cs
@@ -12,32 +12,40 @@ namespace offerStation.Core.Interfaces
     public interface IUnitOfWork : IDisposable
     {
 
-        public IBaseRepository <Admin> Admins { get;  }
-        public IBaseRepository <Customer> Customers { get;  }
+        public IBaseRepository <City> Cities { get; }
+        public IBaseRepository <Admin> Admins { get; }
+        public IBaseRepository <Address> Addresses { get; }
+        public IBaseRepository <Delivery> Deliveries { get; }
+
+        public IBaseRepository <Customer> Customers { get; }
         public IBaseRepository <CustomerCart> CustomerCarts { get; }
-        public IBaseRepository <CustomerOrder> CustomerOrders { get;  }
-        public IBaseRepository <CustomerReview> CustomerReviews { get;  }
-        public IBaseRepository <CustomerCartProduct> CustomerCartProducts { get;  }
-        public IBaseRepository <CustomerOrderProduct> CustomerOrderProducts { get;  }
-        
+        public IBaseRepository <CustomerOrder> CustomerOrders { get; }
+        public IBaseRepository <CustomerReview> CustomerReviews { get; }
+        public IBaseRepository <CustomerCartProduct> CustomerCartProducts { get; }
+        public IBaseRepository <CustomerCartOffer> CustomerCartOffers { get; }
+        public IBaseRepository <CustomerOrderOffer> CustomerOrderOffers { get; }
+        public IBaseRepository <CustomerCardDetails> CustomerCardDetails { get; }
+        public IBaseRepository <CustomerOrderProduct> CustomerOrderProducts { get; }
+
         public IBaseRepository <Owner> Owners { get;  }
         public IBaseRepository <OwnerCart> OwnerCarts { get; }
-        public IBaseRepository <OwnerMenuCategory> OwnerMenus { get;  }
         public IBaseRepository <OwnerOrder> OwnerOrders { get;  }
         public IBaseRepository <OwnerOffer> OwnerOffers { get;  }
-        public IBaseRepository <OwnerReview> OwnerReviews { get;  }
         public IBaseRepository <OwnerProduct> OwnerProducts { get;  }
+        public IBaseRepository <OwnerReview> OwnerReviews { get;  }
         public IBaseRepository <OwnerCategory> OwnerCategories { get;  }
-        public IBaseRepository <OwnerCartProduct> OwnerCartProducts { get;  }
-        public IBaseRepository <OwnerOrderProduct> OwnerOrderProducts { get;  }
-        public IBaseRepository <OwnerOfferProduct> OwnerOfferProducts { get;  }
+        public IBaseRepository <OwnerCardDetails> OwnerCardDetails { get; }
+        public IBaseRepository <OwnerCartProduct> OwnerCartProducts { get; }
+        public IBaseRepository <OwnerMenuCategory> OwnerMenuCategories { get; }
+        public IBaseRepository <OwnerOrderProduct> OwnerOrderProducts { get; }
+        public IBaseRepository <OwnerOfferProduct> OwnerOfferProducts { get; }
         
         public IBaseRepository <Supplier> Suppliers { get;  }
         public IBaseRepository <SupplierOffer> SupplierOffers { get; }
-        public IBaseRepository <SupplierProduct> SupplierProducts { get;  }
-        public IBaseRepository <SupplierCategory> SupplierCategories { get;  }
-        public IBaseRepository <SupplierOfferProduct> SupplierOfferProducts { get;  }
-        public IBaseRepository <SupplierMenuCategory> SupplierMenuCategories { get;  }
+        public IBaseRepository <SupplierProduct> SupplierProducts { get; }
+        public IBaseRepository <SupplierCategory> SupplierCategories { get; }
+        public IBaseRepository <SupplierOfferProduct> SupplierOfferProducts { get; }
+        public IBaseRepository <SupplierMenuCategory> SupplierMenuCategories { get; }
 
         int Complete();
     }
--- offerStation_BackEnd/offerStation.EF/UnitOfWork.cs
+++ offerStation_BackEnd/offerStation.EF/UnitOfWork.cs
@@ -16,56 +16,73 @@ namespace offerStation.EF
     {
         private readonly ApplicationDbContext _context;
 
-        public IBaseRepository<Admin> Admins { get; private set; }
-        public IBaseRepository<Customer> Customers { get; private set; }
-        public IBaseRepository<CustomerCart> CustomerCarts { get; private set; }
-        public IBaseRepository<CustomerOrder> CustomerOrders { get; private set; }
-        public IBaseRepository<CustomerReview> CustomerReviews { get; private set; }
-        public IBaseRepository<CustomerCartProduct> CustomerCartProducts { get; private set; }
-        public IBaseRepository<CustomerOrderProduct> CustomerOrderProducts { get; private set; }
+        public IBaseRepository <City> Cities { get; private set; }
+        public IBaseRepository <Admin> Admins { get; private set; }
+        public IBaseRepository <Address> Addresses { get; private set; } 
+        public IBaseRepository <Delivery> Deliveries { get; private set; }
 
-        public IBaseRepository<Owner> Owners { get; private set; }
-        public IBaseRepository<OwnerCart> OwnerCarts { get; private set; }
-        public IBaseRepository<OwnerMenuCategory> OwnerMenus { get; private set; }
-        public IBaseRepository<OwnerOrder> OwnerOrders { get; private set; }
-        public IBaseRepository<OwnerOffer> OwnerOffers { get; private set; }
-        public IBaseRepository<OwnerReview> OwnerReviews { get; private set; }
-        public IBaseRepository<OwnerProduct> OwnerProducts { get; private set; }
-        public IBaseRepository<OwnerCategory> OwnerCategories { get; private set; }
-        public IBaseRepository<OwnerCartProduct> OwnerCartProducts { get; private set; }
-        public IBaseRepository<OwnerOrderProduct> OwnerOrderProducts { get; private set; }
-        public IBaseRepository<OwnerOfferProduct> OwnerOfferProducts { get; private set; }
+        public IBaseRepository <Customer> Customers { get; private set; }
+        public IBaseRepository <CustomerCart> CustomerCarts { get; private set; }
+        public IBaseRepository <CustomerOrder> CustomerOrders { get; private set; }
+        public IBaseRepository <CustomerReview> CustomerReviews { get; private set; }
+        public IBaseRepository <CustomerCartProduct> CustomerCartProducts { get; private set; }
+        public IBaseRepository <CustomerCartOffer> CustomerCartOffers { get; private set; }
+        public IBaseRepository <CustomerOrderOffer> CustomerOrderOffers { get; private set; }
+        public IBaseRepository <CustomerCardDetails> CustomerCardDetails { get; private set; }
+        public IBaseRepository <CustomerOrderProduct> CustomerOrderProducts { get; private set; }
+
+        public IBaseRepository <Owner> Owners { get; private set; }
+        public IBaseRepository <OwnerCart> OwnerCarts { get; private set; }
+        public IBaseRepository <OwnerOrder> OwnerOrders { get; private set; }
+        public IBaseRepository <OwnerOffer> OwnerOffers { get; private set; }
+        public IBaseRepository <OwnerReview> OwnerReviews { get; private set; }
+        public IBaseRepository <OwnerProduct> OwnerProducts { get; private set; }
+        public IBaseRepository <OwnerCategory> OwnerCategories { get; private set; }
+        public IBaseRepository <OwnerCardDetails> OwnerCardDetails { get; private set; }
+        public IBaseRepository <OwnerCartProduct> OwnerCartProducts { get; private set; }
+        public IBaseRepository <OwnerMenuCategory> OwnerMenuCategories { get; private set; }
+        public IBaseRepository <OwnerOrderProduct> OwnerOrderProducts { get; private set; }
+        public IBaseRepository <OwnerOfferProduct> OwnerOfferProducts { get; private set; }
 
         public IBaseRepository<Supplier> Suppliers { get; private set; }
         public IBaseRepository<SupplierOffer> SupplierOffers { get; private set; }
         public IBaseRepository<SupplierProduct> SupplierProducts { get; private set; }
         public IBaseRepository<SupplierCategory> SupplierCategories { get; private set; }
-        public IBaseRepository<SupplierOfferProduct> SupplierOfferProducts { get; private set; }
         public IBaseRepository<SupplierMenuCategory> SupplierMenuCategories { get; private set; }
+        public IBaseRepository<SupplierOfferProduct> SupplierOfferProducts { get; private set; }
 
         public UnitOfWork(ApplicationDbContext context)
         {
             _context = context;
 
             Admins = new BaseRepository<Admin>(_context);
+            Cities = new BaseRepository<City>(_context);
+            Addresses = new BaseRepository<Address>(_context);
+            Deliveries = new BaseRepository<Delivery>(_context);
+
             Customers = new BaseRepository<Customer>(_context);
             CustomerCarts = new BaseRepository<CustomerCart>(_context);
             CustomerOrders = new BaseRepository<CustomerOrder>(_context);
             CustomerReviews = new BaseRepository<CustomerReview>(_context);
+            CustomerCartOffers = new BaseRepository<CustomerCartOffer>(_context);
+            CustomerOrderOffers = new BaseRepository<CustomerOrderOffer>(_context);
+            CustomerCardDetails = new BaseRepository<CustomerCardDetails>(_context);
             CustomerCartProducts = new BaseRepository<CustomerCartProduct>(_context);
             CustomerOrderProducts = new BaseRepository<CustomerOrderProduct>(_context);
 
             Owners = new BaseRepository<Owner>(_context);
             OwnerCarts = new BaseRepository<OwnerCart>(_context);
-            OwnerMenus = new BaseRepository<OwnerMenuCategory>(_context);
             OwnerOrders = new BaseRepository<OwnerOrder>(_context);
             OwnerOffers = new BaseRepository<OwnerOffer>(_context);
             OwnerReviews = new BaseRepository<OwnerReview>(_context);
             OwnerProducts = new BaseRepository<OwnerProduct>(_context);
             OwnerCategories = new BaseRepository<OwnerCategory>(_context);
+            OwnerCardDetails = new BaseRepository<OwnerCardDetails>(_context);
+            OwnerCartProducts = new BaseRepository<OwnerCartProduct>(_context);
             OwnerCartProducts = new BaseRepository<OwnerCartProduct>(_context);
             OwnerOrderProducts = new BaseRepository<OwnerOrderProduct>(_context);
             OwnerOfferProducts = new BaseRepository<OwnerOfferProduct>(_context);
+            OwnerMenuCategories = new BaseRepository<OwnerMenuCategory>(_context);
 
             Suppliers = new BaseRepository<Supplier>(_context);
             SupplierOffers = new BaseRepository<SupplierOffer>(_context);
