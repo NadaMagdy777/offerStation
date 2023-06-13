12850caa33f20bed6e0d1180cec49d6921d587d3
Elshaymaa Ahmed <elshaymaaahmed@gmail.com>
2023-06-13 11:04:46 +0300

    Create AddOwnerReview endpoint & OwnerReviewController

--- offerStation_BackEnd/offerStation_BackEnd/Controllers/OwnerController.cs
+++ offerStation_BackEnd/offerStation_BackEnd/Controllers/OwnerController.cs
@@ -174,37 +174,7 @@ namespace offerStation.API.Controllers
             }
             return BadRequest(new ApiResponse(500, false, "server error"));
         }
-        [HttpGet("AllOwnersReviews")]
-        public async Task<ActionResult<ApiResponse>> GetAllOwnersReviews()
-        {
-            List<ReviewDto> reviews = await _ownerService.GetAllOwnersReviews();
-            if(reviews is null)
-            {
-                return BadRequest(new ApiResponse(404, false, "null object"));
-            }
-            return Ok(new ApiResponse(200, true, reviews));
-        }
-        [HttpGet("AllCustomerReviewsByOwnerId/id")]
-        public async Task<ActionResult<ApiResponse>> GetAllCustomerReviews(int ownerId)
-        {
-            List<ReviewDto> reviews = await _ownerService.GetAllCustomerReviewsByOwnerId(ownerId);
-
-            if (reviews is null)
-            {
-                return BadRequest(new ApiResponse(404, false, "null object"));
-            }
-            return Ok(new ApiResponse(200, true, reviews));
-        }
-        [HttpDelete("OwnerReview/id")]
-        public async Task<ActionResult<ApiResponse>> DeleteOwnerReview(int id)
-        {
-            bool success = await _ownerService.DeleteReview(id);
-            if (success)
-            {
-                return Ok(new ApiResponse(200, true, success));
-            }
-            return BadRequest(new ApiResponse(500, false, "server error"));
-        }
+        
         [HttpGet("AllMenuCategoriesByOwnerId/id")]
         public async Task<ActionResult<ApiResponse>> GetMenuCategory(int id)
         {
