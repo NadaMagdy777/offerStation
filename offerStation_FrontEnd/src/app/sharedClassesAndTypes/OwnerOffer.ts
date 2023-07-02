ad7c728a124eb6e33e75f82482fb0a90334766a9
ayaAboelela <samerelekaily@gmail.com>
2023-07-02 16:54:50 +0300

    Work on supplier Offers

--- offerStation_FrontEnd/src/app/sharedClassesAndTypes/OwnerOffer.ts
+++ offerStation_FrontEnd/src/app/sharedClassesAndTypes/OwnerOffer.ts
@@ -1,12 +1,12 @@
 
 export class OwnerDetails {
-    "ownerId":number;
-    "createdTime":string;
-    "id":number;
-    "prefPrice":number;
-    "traderImage":string;
-    "name":string;
-    "description":string;
-    "price":number;
-    "image":string;
+    "ownerId": number;
+    "createdTime": string;
+    "id": number;
+    "prefPrice": number;
+    "traderImage": string;
+    "name": string;
+    "description": string;
+    "price": number;
+    "image": string;
 }
--- offerStation_FrontEnd/src/app/sharedClassesAndTypes/OwnerOfferInfo.ts
+++ offerStation_FrontEnd/src/app/sharedClassesAndTypes/OwnerOfferInfo.ts
@@ -1,17 +1,28 @@
 
 
-export class OwnerOfferInfo {
+export class Offer {
     constructor(
 
-        public ownerId: number,
-        public createdTime: string,
         public id: number,
-        public prefPrice: number,
-        public traderImage: string,
+        public ownerId: number,
         public name: string,
         public description: string,
         public price: number,
         public image: string,
+        public createdTime: string,
+        public prefPrice: number,
+        public traderImage: string,
+        public products: Product[],
 
     ) { }
 }
+
+export class Product {
+    constructor(
+        public quantity: number,
+        public productId: number,
+    ) { }
+
+}
+
+
