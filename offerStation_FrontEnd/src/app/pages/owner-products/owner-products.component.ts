814806ec2e828a1f283d382ce47289b80f1c9bbf
ayaAboelela <samerelekaily@gmail.com>
2023-07-06 08:02:14 +0300

    Working on Forms2

--- offerStation_FrontEnd/src/app/pages/owner-products/owner-products.component.ts
+++ offerStation_FrontEnd/src/app/pages/owner-products/owner-products.component.ts
@@ -36,15 +36,7 @@ export class OwnerProductsComponent implements OnInit {
   categories!: ownerCategory[]
   category!: ownerCategory
 
-  productForm: any = this.fb.group({
-    name: ['', [Validators.required]],
-    description: ['', [Validators.required]],
-    price: ['', [Validators.required]],
-    discount: ['', [Validators.required]],
-    discountPrice: [''],
-    image: [''],
-    categoryId: ['', [Validators.required]],
-  });
+  productForm: FormGroup;
 
   constructor(
     private fb: FormBuilder,
@@ -52,24 +44,34 @@ export class OwnerProductsComponent implements OnInit {
     private _imageService: ImageService,
     private activatedroute: ActivatedRoute) {
 
-    // this.productForm.get('name')?.valueChanges.subscribe((data) => {
-    //   this.ownerProduct.name = data;
-    // });
-    // this.productForm.get('description')?.valueChanges.subscribe((data) => {
-    //   this.ownerProduct.description = data;
-    // });
-    // this.productForm.get('price')?.valueChanges.subscribe((data) => {
-    //   this.ownerProduct.price = data;
-    // });
-    // this.productForm.get('discount')?.valueChanges.subscribe((data) => {
-    //   this.ownerProduct.discount = data;
-    // });
-    // this.productForm.get('image')?.valueChanges.subscribe((data) => {
-    //   this.ownerProduct.image = data;
-    // });
-    // this.productForm.get('categoryId')?.valueChanges.subscribe((data) => {
-    //   this.ownerProduct.categoryId = data;
-    // });
+    this.productForm = this.fb.group({
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      discount: ['', [Validators.required]],
+      discountPrice: [''],
+      image: [''],
+      categoryId: ['', [Validators.required]],
+    });
+
+    this.productForm.get('name')?.valueChanges.subscribe((data) => {
+      this.ownerProduct.name = data;
+    });
+    this.productForm.get('description')?.valueChanges.subscribe((data) => {
+      this.ownerProduct.description = data;
+    });
+    this.productForm.get('price')?.valueChanges.subscribe((data) => {
+      this.ownerProduct.price = data;
+    });
+    this.productForm.get('discount')?.valueChanges.subscribe((data) => {
+      this.ownerProduct.discount = data;
+    });
+    this.productForm.get('image')?.valueChanges.subscribe((data) => {
+      this.ownerProduct.image = data;
+    });
+    this.productForm.get('categoryId')?.valueChanges.subscribe((data) => {
+      this.ownerProduct.categoryId = data;
+    });
   }
 
   ngOnInit(): void {
@@ -80,11 +82,11 @@ export class OwnerProductsComponent implements OnInit {
 
     this.LoadData();
 
-    this._ownerService.getMenuCategorybyOwnerId(1).subscribe({
+    this._ownerService.getMenuCategorybyOwnerId(this.id).subscribe({
       next: data => {
 
         let dataJson = JSON.parse(JSON.stringify(data))
-        this.categories = dataJson.data
+        this.categories = dataJson.data;
       },
       error: (error: any) => this.errorMessage = error,
     });
@@ -96,24 +98,24 @@ export class OwnerProductsComponent implements OnInit {
         console.log(data);
         let dataJson = JSON.parse(JSON.stringify(data))
         this.ProductList = dataJson.data;
+        this.ProductList.forEach((product: ProductInfo) => {
+          product.image = this._imageService.base64ArrayToImage(product.image)
+        });
 
       },
       error: error => this.errorMessage = error
     });
   }
 
-  OnImageLoad(image: any) {
-    this.imageUrl = this._imageService.base64ArrayToImage(image);
-  }
-
   SubmitData() {
 
     console.log(this.productForm.value);
-    this._ownerService.AddProduct(this.id, this.productForm.value).subscribe({
+    this._ownerService.AddProduct(this.id, this.ownerProduct).subscribe({
       next: data => {
         console.log(data);
         this.LoadData()
         this.onCloseProductHandled();
+        this.productForm.reset();
       },
       error: (error: any) => this.errorMessage = error,
     });
@@ -132,7 +134,7 @@ export class OwnerProductsComponent implements OnInit {
   }
 
   UpdateProduct() {
-    this._ownerService.UpdateProduct(this.ownerProduct.id, this.productForm.value).subscribe({
+    this._ownerService.UpdateProduct(this.ownerProduct.id, this.ownerProduct).subscribe({
       next: data => {
         this.ProductList[this.index] = this.productForm.value;
         this.onCloseEditProductHandled();
